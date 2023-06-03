20949250b9b2918b6b7e4240e4e5ec3c659f203e
Loading31115 <132940697+Loading31115@users.noreply.github.com>
2023-06-03 19:58:37 +0200

    Update Wines.php

--- Wines.php
+++ Wines.php
@@ -36,23 +36,51 @@
                  <option value="" disabled selected>Sort</option>
                  <option value="Price(Highest - Lowest)" id="SortByModel">Sort by Prices(Highest - Lowest)</option>
                  <option value="Price(Lowest - Highest)" id="SortByBrand">Sort by Prices(Lowest - Highest)</option>
-                 <option value="Wines(Newest)" id="SortByCarType">Sort by Wines(Newest)</option>
+                 <option value="Alcohol Content(Highest - Lowest)" id="SortByCarType">Sort by Alcohol Content(Highest - Lowest)</option>
+                 <option value="Alcohol Content(Lowest - Highest)" id="SortByCarType">Sort by Alcohol Content(Lowest - Highest)</option>
                </select>  
              </div>
              <div>
-                <button id="Refine" class="refine" onclick="refine()">Refine</button>
-                <?php
-                if(isset($_COOKIE['api_key'])&& $_COOKIE['validated']){
-                    echo "<button id='Refine' class='refine' onclick='checkFilters()'>Save preferences</button>";
-                }
-                ?>
+                <button id="Refine" class="refine" onclick="refine()">Refine</button><br>
+                <button id="Refine" class="refine" onclick="refine()">Update Wines</button>
              </div>
              
         </div>
         <!--Cars Section-->
         <div id="car-section"><!--Parent-->
           <div class="car-wrapper">
-            
+            <!-- <div class="car">
+               <img src="img/wine1.jpg" alt="wine1">
+               <h2>JACOB'S CREEK</h2>
+               <h3>SHIRAZ</h3>
+               <li>Price: R189.99</li>
+               <li>Bottle Size: 750ml</li>
+               <li>pH: 3.6 </li>
+               <li>Alcohol Content: 11%</li>
+               <li>Region: Cape Town, South Africa</li>
+            </div>
+
+            <div class="car">
+               <img src="img/wine2.jpg" alt="wine1">
+               <h2>Bonterra</h2>
+               <h3>ROSÉ</h3>
+               <li>Price: R124.99</li>
+               <li>Bottle Size: 750ml</li>
+               <li>pH: 3.0 </li>
+               <li>Alcohol Content: 10.4%</li>
+               <li>Region: Milan, Italy</li>
+            </div>
+
+            <div class="car">
+               <img src="img/wine3.jpg" alt="wine1">
+               <h2>KLEINE ZALZE</h2>
+               <h3>CARBANET SAUVIGNON</h3>
+               <li>Price: R449.99</li>
+               <li>Bottle Size: 750ml</li>
+               <li>pH: 3.8 </li>
+               <li>Alcohol Content: 11.8%</li>
+               <li>Region: Stellenbosch, South Africa</li>
+            </div> -->
           </div>
           
         </div>
