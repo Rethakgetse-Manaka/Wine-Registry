dbaf31c3e7ce476b6702b585180a394bc54f2f9f
Loading31115 <132940697+Loading31115@users.noreply.github.com>
2023-06-03 20:15:35 +0200

    Update header.php

--- globals/header.php
+++ globals/header.php
@@ -6,14 +6,10 @@
 <script src="js/preferences.js" defer></script>
 <script src="js/Search_bar.js" defer></script>
 <nav>
-            <div class = "logo">
-               <img id="Logo" src="img/Full_Logo.png" alt="Car Paradise">
-               <a href="Cars.html"></a>
-            </div>
             <ul class="nav-links">
                <li><a id=Cars-link href="Wines.php" onclick="PageLoad();toggleActive()">Wines</a></li>
-               <li><a id=Brands-link href="Wine_Destinations.php" onclick="toggleActive()">Best Destinations</a></li>
-               <li><a id=Find-link href="FindWine.php" onclick="toggleActive()">Find Wine</a></li>
+               <li><a id=Brands-link href="Wine_Destinations.php" onclick="toggleActive()">Wineries</a></li>
+               <li><a id=Find-link href="Region.php" onclick="toggleActive()">Regions</a></li>
                <?php
                if(isset($_COOKIE['validated'])&& $_COOKIE['validated']){
                   $id = $_COOKIE['api_key'];
@@ -21,8 +17,9 @@
                   echo "<li><a href=''>User Name: ".$name."</a></li>";
                   echo "<li><a href='./globals/logout.php'id=LogOut'>Logout</a></li>";
                }else{
-                     echo '<li><a href="./login.php">Login</a></li>';
-                     echo '<li><a href="./signup.php">Sign Up</a></li>';
+                     echo '<li><a href="./login.php">User-Login</a></li>';
+                     echo '<li><a href="./admin-login.php">Admin-Login</a></li>';
+                     echo '<li><a href="./signup.php">Sign-Up</a></li>';
                }
                
                 ?>
